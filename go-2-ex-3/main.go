6c2859b4110d5148c237f62c005e29448f05b5f6
Domenic <domenic.troxler@gmx.ch>
2024-12-02 08:37:38 +0100

    exercise 2.2

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -1,16 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// TODO: create a map called "modules"
+
+	modules := map[int]string{
+		104: "Modul 104 ist genial",
+		117: "Modul 117 ist hammer",
+		346: "Modul 346 ist mega",
+	}
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
-	// TODO: delete one
-	// TODO: add one
-	// TODO: replace one
+	delete(modules, 117) // TODO: delete one
+	modules[123] = "Modul 123" // TODO: add one
+	modules[104] = "Modul 104 ist supergenial"// TODO: replace one
 	fmt.Println(modules)
 }
