1eda2dc0948de19ea685d986520f7748b6d9613b
Domenic <domenic.troxler@gmx.ch>
2024-12-09 09:27:52 +0100

    exercise 4.1

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,24 @@
 package main
 
+
+import (
+	"fmt"
+	"math"
+)
+
 // TODO: implement the function computeGrade
+func computeGrade(gotPoints, maxPoints float64) float64 {
+	if maxPoints == 0 {
+		return 1.0
+	}
+
+	grade := 1.0 + 5.0*(gotPoints/maxPoints)
+	return math.Min(math.Max(grade, 1.0), 6.0)
+}
 
 func main() {
 	// TODO: call the function computeGrade
+	fmt.Println(computeGrade(17.5, 20.0)) 
+	fmt.Println(computeGrade(12.0, 20.0)) 
+	fmt.Println(computeGrade(3.0, 28.0)) 
 }
